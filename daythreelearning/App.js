60a6cbb1ed553b1e9bbd4684050847e371006451
prabhat1510 <prabhatc@gmail.com>
2022-11-28 12:45:07 +0530

    Added Section List example

--- daythreelearning/App.js
+++ daythreelearning/App.js
@@ -4,11 +4,12 @@ import PizzaTranslator from './PizzaTranslator';
 import styles from './Styles';
 import ScrollViewApp from './ScrollViewApp';
 import FlatListBasics from './FlatListBasics';
+import SectionListBasics from './SetionListBasics';
 const App=() =>{
   return (
-    <View style={styles.container}>
-      <Text>Inside App we will render Hello World Component ! </Text>
-     <FlatListBasics /> 
+    <View>
+      
+      <SectionListBasics/>
     
     </View>
   )
