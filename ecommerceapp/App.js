4ef2504c0b5513ef54dc543c49da8a568b8b5564
prabhat1510 <prabhatc@gmail.com>
2022-11-29 11:58:44 +0530

    updated app js with navigator

--- ecommerceapp/App.js
+++ ecommerceapp/App.js
@@ -4,9 +4,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { ProductsList } from './screens/ProductsList';
+import { ProductDetails } from './screens/ProductDetails.js';
+import { Cart } from './screens/Cart.js';
+import { CartIcon } from './components/CartIcon.js';
+import { CartProvider } from './CartContext.js';
 
 const Stack = createNativeStackNavigator();
-
+/**
 function App() {
   return (
     <NavigationContainer>
@@ -16,5 +20,40 @@ function App() {
     </NavigationContainer>
   );
 }
+ */
+function App() {
+  return (
+    <CartProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Products' component={ProductsList} 
+          options={({ navigation }) => ({
+            title: 'Products',
+            headerTitleStyle: styles.headerTitle,
+            headerRight: () => <CartIcon navigation={navigation}/>
+          })}/>
+          <Stack.Screen name='ProductDetails' component={ProductDetails} 
+          options={({ navigation }) => ({
+            title: 'Product details',
+            headerTitleStyle: styles.headerTitle,
+            headerRight: () => <CartIcon navigation={navigation}/>,
+          })} />
+          <Stack.Screen name='Cart' component={Cart} 
+          options={({ navigation }) => ({
+            title: 'My cart',
+            headerTitleStyle: styles.headerTitle,
+            headerRight: () => <CartIcon navigation={navigation}/>,
+          })} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CartProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerTitle: {
+    fontSize: 20
+  }
+});
 
 export default App;
